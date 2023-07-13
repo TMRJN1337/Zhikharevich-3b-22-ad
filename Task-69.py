ff3a4ba8783afaaae698862c106762a0c3593f95
Timur Zhikharevich <123844650+TMRJN1337@users.noreply.github.com>
2023-07-13 22:16:40 +0300

    Update Task-69.py

--- Task-69.py
+++ Task-69.py
@@ -1,20 +1,36 @@
 import sqlite3
 
-conn = sqlite3.connect('employees.db')
-cursor = conn.cursor()
-cursor.execute("""CREATE TABLE IF NOT EXISTS employees(
-    name TEXT,
-    title TEXT,
-    salary REAL
-)""")
+def create_employees_table():
+    conn = sqlite3.connect('employees.db')
+    cursor = conn.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS employees(
+                        name TEXT, 
+                        title TEXT, 
+                        salary REAL
+                    )""")
+    conn.commit()
+    conn.close()
 
-employees = [('Anton', 'manager', 20000), ['Vlad', 'director', 40000],
-             ('Irina', 'cleaning', 20000), ('Kirill', 'manager', 20000)]
-cursor.executemany("INSERT INTO employees VALUES(?, ?, ?);", employees)
-conn.commit()
+def insert_employees_data():
+    conn = sqlite3.connect('employees.db')
+    cursor = conn.cursor()
+    employees = [('Arkadiy', 'manager', 35000), 
+                 ('George', 'director', 60000), 
+                 ('Pavel', 'cleaning', 10000), 
+                 ('Michael', 'manager', 30000)]
+    cursor.executemany("INSERT INTO employees VALUES(?, ?, ?);", employees)
+    conn.commit()
+    conn.close()
 
-cursor.execute("SELECT * FROM employees WHERE title='manager';")
-all_employees = cursor.fetchall()
-for employee in all_employees:
-    print(f'Имя: {employee[0]}, Должность: {employee[1]}, Зарплата: {employee[2]}')
-conn.close()
+def select_managers():
+    conn = sqlite3.connect('employees.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM employees WHERE title='manager';")
+    all_employees = cursor.fetchall()
+    for employee in all_employees:
+        print(f'Имя: {employee[0]}, Должность: {employee[1]}, Зарплата: {employee[2]}')
+    conn.close()
+
+create_employees_table()
+insert_employees_data()
+select_managers()
